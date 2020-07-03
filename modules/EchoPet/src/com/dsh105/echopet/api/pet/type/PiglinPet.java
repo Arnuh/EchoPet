dd7acedb73ef97fc11325836b0233a3405c895ad
Arnah <github@arnah.ca>
2020-07-02 19:58:27 -0600

    Implement Piglin #81

---
+++ modules/EchoPet/src/com/dsh105/echopet/api/pet/type/PiglinPet.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of EchoPet.
+ *
+ * EchoPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EchoPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EchoPet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.echopet.api.pet.type;
+
+import com.dsh105.echopet.api.pet.AgeablePet;
+import com.dsh105.echopet.compat.api.entity.EntityPetType;
+import com.dsh105.echopet.compat.api.entity.PetType;
+import com.dsh105.echopet.compat.api.entity.type.nms.IEntityPiglinPet;
+import com.dsh105.echopet.compat.api.entity.type.pet.IPiglinPet;
+import org.bukkit.entity.Player;
+
+@EntityPetType(petType = PetType.PIGLIN)
+public class PiglinPet extends AgeablePet implements IPiglinPet{
+	
+	private boolean dancing;
+	
+	public PiglinPet(Player owner){
+		super(owner);
+	}
+	
+	@Override
+	public void setDancing(boolean flag){
+		this.dancing = flag;
+		((IEntityPiglinPet) getEntityPet()).setDancing(flag);
+	}
+}
