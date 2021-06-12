fce5ff1a1d4bb87ff801f7b6b666ca06ebe8524b
Arnah <github@arnah.ca>
2021-06-12 03:06:21 -0600

    Initial copy of 1.16 renamed to 1.17

---
+++ modules/v1_17/src/com/dsh105/echopet/compat/nms/v1_17_R1/entity/type/EntityFishPet.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of EchoPet.
+ * EchoPet is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * EchoPet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with EchoPet. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.dsh105.echopet.compat.nms.v1_17_R1.entity.type;
+
+import com.dsh105.echopet.compat.api.entity.IPet;
+import com.dsh105.echopet.compat.api.entity.type.nms.IEntityFishPet;
+import net.minecraft.server.v1_17_R1.DataWatcher;
+import net.minecraft.server.v1_17_R1.DataWatcherObject;
+import net.minecraft.server.v1_17_R1.DataWatcherRegistry;
+import net.minecraft.server.v1_17_R1.EntityInsentient;
+import net.minecraft.server.v1_17_R1.EntityTypes;
+import net.minecraft.server.v1_17_R1.World;
+
+public abstract class EntityFishPet extends EntityWaterAnimalPet implements IEntityFishPet{
+	
+	private static final DataWatcherObject<Boolean> b = DataWatcher.a(EntityFishPet.class, DataWatcherRegistry.i);// "FromBucket". Prevents the fish from despawning.
+	
+	public EntityFishPet(EntityTypes<? extends EntityInsentient> type, World world){
+		super(type, world);
+	}
+	
+	public EntityFishPet(EntityTypes<? extends EntityInsentient> type, World world, IPet pet){
+		super(type, world, pet);
+	}
+	
+	protected void initDatawatcher(){
+		super.initDatawatcher();
+		this.datawatcher.register(b, Boolean.valueOf(false));
+	}
+	
+	protected String getStepSound(){
+		return "entity." + pet.getPetType().getMinecraftName() + ".flop";
+	}
+}
